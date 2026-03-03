97aa06a1835244a5f656daa50dab6f27ee220f72
mrtingly <116870308+mrtingly@users.noreply.github.com>
2026-03-04 02:20:22 +0700

    Update configloader.js

--- js/configloader.js
+++ js/configloader.js
@@ -1,5 +1,5 @@
 export async function loadJSON(path){
-  const res = await fetch(path, {cache:"no-store"});
-  if(!res.ok) throw new Error("Failed load: " + path);
+  const res = await fetch(path, { cache: "no-store" });
+  if(!res.ok) throw new Error(`Failed to load ${path}`);
   return await res.json();
 }
