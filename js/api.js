c97f6cfc3b7c53e2177d3fbf09587f8bb47d215e
mrtingly <116870308+mrtingly@users.noreply.github.com>
2026-03-04 04:43:37 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-export async function submitOrder(endpointUrl, payload) {
+export const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbzKUf38mPTywUqOLXHqDPIeBH7_VV2w42CbLc5rg72dufSKAUd2XDkFhA4tXtjy_LMrFQ/exec"; {
   const fd = new FormData();
   fd.append("payload", JSON.stringify(payload)); // ส่งเป็นฟิลด์เดียว
 
