12e1e6b746743081c8fd3dc32817f8d2a40bfe76
mrtingly <116870308+mrtingly@users.noreply.github.com>
2026-05-12 17:05:22 +0700

    Update admin-guard.js

--- js/admin-guard.js
+++ js/admin-guard.js
@@ -2,17 +2,15 @@ const adminId = localStorage.getItem("admin_id");
 const adminRole = localStorage.getItem("admin_role");
 
 if(!adminId){
-  window.location.href = "admin-login.html";
+  window.location.href = "login.html";
 }
 
 if(adminRole !== "Admin" && adminRole !== "Owner"){
-  alert("ไม่มีสิทธิ์เข้าใช้งาน");
-
   localStorage.removeItem("admin_id");
   localStorage.removeItem("admin_name");
   localStorage.removeItem("admin_role");
 
-  window.location.href = "admin-login.html";
+  window.location.href = "login.html";
 }
 
 function adminLogout(){
@@ -20,5 +18,5 @@ function adminLogout(){
   localStorage.removeItem("admin_name");
   localStorage.removeItem("admin_role");
 
-  window.location.href = "admin-login.html";
+  window.location.href = "login.html";
 }
